4744b624b91daa8eaa6a228b97e5e96a21192793
AJ Generoso <ajgeneroso@AJs-MacBook-Air.lan>
2024-03-04 12:27:48 -0500

    jsx stuff

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ExerciseSchedule from './components/ExerciseSchedule';
+
+function App() {
+  return (
+    <div className="exercise-schedule">
+      <ExerciseSchedule/>
+    </div>
+  );
+}
+
+export default App;
