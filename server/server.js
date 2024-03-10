bf533587162fe8ba64ab53ba7070e45b700f1423
Mike Neal <152903947+Mike-Neal@users.noreply.github.com>
2024-03-09 23:51:44 -0500

    updated pkg jsons and server files

--- server/server.js
+++ server/server.js
@@ -1,22 +1,65 @@
-const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+// const express = require('express');
+// const db = require('./config/connection');
+// const routes = require('./routes');
 
-const cwd = process.cwd();
+// const cwd = process.cwd();
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+// const activity = cwd.includes('01-Activities')
+//   ? cwd.split('01-Activities')[1]
+//   : cwd;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server for ${activity} is running on port ${PORT}!`);
+//   });
+// });
+
+
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('01-Activities')[1]
-  : cwd;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server for ${activity} is running on port ${PORT}!`);
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(server));
+
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  } 
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
